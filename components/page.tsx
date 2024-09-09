66a5c0646090b5606aa740573a38a13da96c346a
Govind-Jangra <govind70821@gmail.com>
2024-09-09 21:26:21 +0530

    chore: add spacing

--- components/page.tsx
+++ components/page.tsx
@@ -273,6 +273,7 @@ export function Page() {
   return (
     <div className="flex flex-col h-screen bg-gray-900 text-white">
       <SignedIn>
+        
       {/* Header */}
       <header className="fixed top-0 left-0 right-0 z-10 flex items-center justify-between px-4 py-2 bg-gray-800 text-white">
         <div className="flex items-center">
