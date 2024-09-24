13a838a76bd486971ea0241e1b7b123fef57e39e
Govind-Jangra <govind70821@gmail.com>
2024-09-24 19:21:44 +0530

    feat: add subscribe to newsletter during signup

--- app/signup/[[...signup]]/page.tsx
+++ app/signup/[[...signup]]/page.tsx
@@ -1,12 +1,36 @@
+"use client"
+import { useState } from 'react';
 import { SignUp } from '@clerk/nextjs';
 
-
 export default function SignUpPage() {
-	return (
-		<div className="relative h-screen w-full overflow-hidden bg-gray-900">
-			<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-				<SignUp />
-			</div>
-		</div>
-	);
+  const [newsletter, setNewsletter] = useState(true); 
+
+  const handleNewsletterChange = (event:any) => {
+    setNewsletter(event.target.checked); 
+  };
+
+  return (
+    <div className="relative h-screen w-full overflow-hidden bg-gray-900">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+        
+        <div className="flex flex-col items-center mb-4">
+          <label className="text-white font-thin">
+            <input
+              type="checkbox"
+              checked={newsletter}
+              onChange={handleNewsletterChange}
+              className="mr-2"
+            />
+            Subscribe to our newsletter
+          </label>
+        </div>
+        
+        
+        <SignUp
+          signInUrl={`${process.env.NEXT_PUBLIC_BASE_URL}/signin`}
+          unsafeMetadata={{ newsletter }} 
+        />
+      </div>
+    </div>
+  );
 }
